bfad77a3f017cf19fa1c7e09dfe927233aa12c08
jarrebear <battistini.jarrett@gmail.com>
2026-05-20 03:24:09 +0000

    Added suggested actions for obstacle detection

--- mars_rover_tasks/src/subscriber_obstacle_detector.cpp
+++ mars_rover_tasks/src/subscriber_obstacle_detector.cpp
@@ -31,13 +31,50 @@ private:
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscriber_;
 
   void laserscan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
-    // Find the minimum distance in the ranges array
-    float min_distance =
-        *std::min_element(msg->ranges.begin(), msg->ranges.end());
+    // Distance at which we will want to turn to avoid obstacle
+    float detection_distance{0.8};
 
+    // Find the minimum distance in the ranges array
+    float min_distance_lr{min_laserscan_range(msg, 167, 200)};
+    float min_distance_l{min_laserscan_range(msg, 134, 166)};
+    float min_distance_fl{min_laserscan_range(msg, 101, 133)};
+    float min_distance_fr{min_laserscan_range(msg, 67, 100)};
+    float min_distance_r{min_laserscan_range(msg, 34, 66)};
+    float min_distance_rr{min_laserscan_range(msg, 0, 33)};
     // Log the minimum distance value
-    RCLCPP_INFO(this->get_logger(), "Minimum distance: %.2f meters",
-                min_distance);
+    RCLCPP_INFO(this->get_logger(), "Left_Rear: %.2f meters", min_distance_lr);
+    RCLCPP_INFO(this->get_logger(), "Left: %.2f meters", min_distance_l);
+    RCLCPP_INFO(this->get_logger(), "Front_Left: %.2f meters", min_distance_fl);
+    RCLCPP_INFO(this->get_logger(), "Front_Right: %.2f meters",
+                min_distance_fr);
+    RCLCPP_INFO(this->get_logger(), "Right: %.2f meters", min_distance_r);
+    RCLCPP_INFO(this->get_logger(), "Right_Rear: %.2f meters", min_distance_rr);
+    if (min_distance_fl <= detection_distance) {
+      RCLCPP_INFO(this->get_logger(), "Action Suggested = Turn Right");
+    } else if (min_distance_fr <= detection_distance) {
+      RCLCPP_INFO(this->get_logger(), "Action Suggested = Turn Left");
+    } else if (min_distance_l <= detection_distance) {
+      RCLCPP_INFO(this->get_logger(),
+                  "Action Suggested = Go Forwards turning slightly right ( "
+                  "proportional to the angle of detection)");
+    } else if (min_distance_r <= detection_distance) {
+      RCLCPP_INFO(this->get_logger(),
+                  "Action Suggested = Go Forwards turning slightly left ( "
+                  "proportional to the angle of detection)");
+    } else if (min_distance_lr <= detection_distance) {
+      RCLCPP_INFO(this->get_logger(), "Action Suggested = Go Forwards");
+    } else if (min_distance_rr <= detection_distance) {
+      RCLCPP_INFO(this->get_logger(), "Action Suggested = Go Forwards");
+    } else {
+      RCLCPP_INFO(this->get_logger(), "Action Suggested = Go Forwards");
+    }
+  }
+
+  float min_laserscan_range(const sensor_msgs::msg::LaserScan::SharedPtr msg,
+                            int begin, int end) {
+    float min_distance = *std::min_element(msg->ranges.begin() + begin,
+                                           msg->ranges.begin() + end);
+    return min_distance;
   }
 };
 
